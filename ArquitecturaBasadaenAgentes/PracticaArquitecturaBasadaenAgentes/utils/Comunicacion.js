32072e12d759e419a8f0050100f2a33af21bca92
DanielaCG8 <128733931+DanielaCG8@users.noreply.github.com>
2024-07-19 22:28:10 -0500

    Delete ArquitecturaBasadaenAgentes directory

--- ArquitecturaBasadaenAgentes/PracticaArquitecturaBasadaenAgentes/utils/Comunicacion.js
+++
@@ -1,8 +0,0 @@
-function enviarMensaje(origen, destino, mensaje) {
-    console.log(`Mensaje enviado desde Agente ${origen} a Agente ${destino}: ${mensaje}`);
-  }
-  
-  module.exports = {
-    enviarMensaje
-  };
-  
