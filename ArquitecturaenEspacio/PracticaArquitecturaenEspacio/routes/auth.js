266672e4b69d6c2b29fa15c769fc157f861aae9e
DanielaCG8 <128733931+DanielaCG8@users.noreply.github.com>
2024-07-19 22:28:48 -0500

    Delete ArquitecturaenEspacio directory

--- ArquitecturaenEspacio/PracticaArquitecturaenEspacio/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-// Definir rutas para autenticación
-router.post('/login', authController.login);
-router.post('/register', authController.register);
-
-module.exports = router;
